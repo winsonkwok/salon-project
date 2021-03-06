b94c734bdd4cde4f60fd79395de8b0ee0b96313e
Winson <greatwinson@gmail.com>
2021-03-06 14:21:28 +0800

    react ui added

---
+++ salon-ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+        <p style={{padding: "150px"}}>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ salon-ui/src/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                Ar Salon & Day Spa
+            </div>
+        );
+    }
+}
+
+export default NavBar;
